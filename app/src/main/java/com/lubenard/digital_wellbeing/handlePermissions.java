2dc1ecf5450a7f97b895896a0c0e49044fda5a0f
lubenard <lubenard@student.42.fr>
2020-08-30 11:28:56 +0200

    No need to relaunch the app when authorising notification

--- app/src/main/java/com/lubenard/digital_wellbeing/handlePermissions.java
+++ app/src/main/java/com/lubenard/digital_wellbeing/handlePermissions.java
@@ -16,6 +16,8 @@ import androidx.fragment.app.Fragment;
 
 public class handlePermissions extends Fragment {
 
+    public static final String TAG = "handlePermission";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,31 +29,33 @@ public class handlePermissions extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        /*int mode;
-
-        AppOpsManager appOps = (AppOpsManager) getContext().getSystemService(Context.APP_OPS_SERVICE);
-
-        //Handling different android permissions version
-        if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-            mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), getContext().getPackageName());
-        } else {
-            mode = appOps.unsafeCheckOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), getContext().getPackageName());
-        }
-
-        if (mode == AppOpsManager.MODE_ALLOWED) {
-            LicenseFragment aboutFrag = new LicenseFragment();
-            getActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(android.R.id.content, aboutFrag, "findThisFragment")
-                    .addToBackStack(null)
-                    .commit();
-        }*/
-
         Button permissionButton = view.findViewById(R.id.permission_button);
 
+        // I do not like the idea of having to check twice for authorisation (one at launch + one here)
+        // TODO: Find a way to change to autodetect auth, so no need to click a second time on the button ?
         permissionButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
-                startActivity(intent);
+                int mode;
+
+                AppOpsManager appOps = (AppOpsManager) getContext().getSystemService(Context.APP_OPS_SERVICE);
+
+                if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
+                    mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), getContext().getPackageName());
+                else
+                    mode = appOps.unsafeCheckOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), getContext().getPackageName());
+
+                if (mode == AppOpsManager.MODE_ALLOWED) {
+                    MainFragment mainFragment = new MainFragment();
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(android.R.id.content, mainFragment, "findThisFragment")
+                            .addToBackStack(null)
+                            .commit();
+                }
+                else
+                {
+                    Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+                    startActivity(intent);
+                }
             }
         });
     }
